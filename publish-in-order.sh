b26114405c55355f8a2f2fc94a6bd3717181ce61
Konrad Pagacz <konrad.pagacz@gmail.com>
2024-11-09 21:23:34 +0200

    feat: fixed the publishing script

--- publish-in-order.sh
+++ publish-in-order.sh
@@ -3,43 +3,51 @@
 
 # Get the version of the crate
 get_local_version() {
-  # Parametrize by the name of the crate
   cargo metadata --format-version=1 --no-deps |
     jq --arg name "$1" '.packages[] | select (.name == $name) | .version' --raw-output
 }
 
 # Get the version from crates.io using cargo
 get_crates_version() {
-  cargo search $1 | grep $1 | cut -d '"' -f 2
+  cargo search $1 | grep "$1 =" | cut -d '"' -f 2
 }
 
 local_version=$(get_local_version "tergo-tokenizer")
 crates_version=$(get_crates_version "tergo-tokenizer")
-if [ $local_version != $crates_version] then
+echo "tergo-tokenizer: local=$local_version, crates=$crates_version"
+if [[ $local_version != $crates_version ]]; then
+  echo "Publishing tergo-tokenizer"
   cargo publish -p tergo-tokenizer
 fi
 
 local_version=$(get_local_version "tergo-parser")
 crates_version=$(get_crates_version "tergo-parser")
-if [ $local_version != $crates_version] then
+echo "tergo-parser: local=$local_version, crates=$crates_version"
+if [[ $local_version != $crates_version ]]; then
+  echo "Publishing tergo-parser"
   cargo publish -p tergo-parser
 fi
 
 local_version=$(get_local_version "tergo-formatter")
 crates_version=$(get_crates_version "tergo-formatter")
-if [ $local_version != $crates_version] then
+echo "tergo-formatter: local=$local_version, crates=$crates_version"
+if [[ $local_version != $crates_version ]]; then
+  echo "Publishing tergo-formatter"
   cargo publish -p tergo-formatter
 fi
 
 local_version=$(get_local_version "tergo-lib")
 crates_version=$(get_crates_version "tergo-lib")
-if [ $local_version != $crates_version] then
+echo "tergo-lib: local=$local_version, crates=$crates_version"
+if [[ $local_version != $crates_version ]]; then
+  echo "Publishing tergo-lib"
   cargo publish -p tergo-lib
 fi
 
 local_version=$(get_local_version "tergo")
 crates_version=$(get_crates_version "tergo")
-if [ $local_version != $crates_version] then
+echo "tergo: local=$local_version, crates=$crates_version"
+if [[ $local_version != $crates_version ]]; then
+  echo "Publishing tergo"
   cargo publish -p tergo
 fi
-
