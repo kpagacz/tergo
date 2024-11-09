275f15fc2070871d4a58ff4e5717b3aa19558479
Konrad Pagacz <konrad.pagacz@gmail.com>
2024-11-09 21:05:06 +0200

    feat: check the crates version before publishing a package

--- publish-in-order.sh
+++ publish-in-order.sh
@@ -1,5 +1,45 @@
-cargo publish -p tergo-tokenizer
-cargo publish -p tergo-parser
-cargo publish -p tergo-formatter
-cargo publish -p tergo-lib
-cargo publish -p tergo
+# If the local version is ahead of the crates
+# version - publish
+
+# Get the version of the crate
+get_local_version() {
+  # Parametrize by the name of the crate
+  cargo metadata --format-version=1 --no-deps |
+    jq --arg name "$1" '.packages[] | select (.name == $name) | .version' --raw-output
+}
+
+# Get the version from crates.io using cargo
+get_crates_version() {
+  cargo search $1 | grep $1 | cut -d '"' -f 2
+}
+
+local_version=$(get_local_version "tergo-tokenizer")
+crates_version=$(get_crates_version "tergo-tokenizer")
+if [ $local_version != $crates_version] then
+  cargo publish -p tergo-tokenizer
+fi
+
+local_version=$(get_local_version "tergo-parser")
+crates_version=$(get_crates_version "tergo-parser")
+if [ $local_version != $crates_version] then
+  cargo publish -p tergo-parser
+fi
+
+local_version=$(get_local_version "tergo-formatter")
+crates_version=$(get_crates_version "tergo-formatter")
+if [ $local_version != $crates_version] then
+  cargo publish -p tergo-formatter
+fi
+
+local_version=$(get_local_version "tergo-lib")
+crates_version=$(get_crates_version "tergo-lib")
+if [ $local_version != $crates_version] then
+  cargo publish -p tergo-lib
+fi
+
+local_version=$(get_local_version "tergo")
+crates_version=$(get_crates_version "tergo")
+if [ $local_version != $crates_version] then
+  cargo publish -p tergo
+fi
+
