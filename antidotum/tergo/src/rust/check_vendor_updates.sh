696a6d61bb037c324d8d69f24e6f74516cac6d30
Konrad Pagacz <konrad.pagacz@gmail.com>
2024-11-24 12:02:57 +0100

    another try at comparing vendored versions

--- antidotum/tergo/src/rust/check_vendor_updates.sh
+++ antidotum/tergo/src/rust/check_vendor_updates.sh
@@ -5,22 +5,10 @@ cargo vendor
 mkdir repo_vendor
 tar -xvf vendor.tar.xz -C repo_vendor
 
-# find ./vendor/ -type f -exec md5sum {} + | sort -k 2 | cut -d" " -f1 >downloaded
-# find ./repo_vendor/vendor/ -type f -exec md5sum {} + | sort -k 2 | cut -d" " -f1 >in_repo
-#
-# if [ ! -f downloaded ] || [ ! -f in_repo ]; then
-#   echo "Error: downloaded or in_repo file not found"
-#   exit 1
-# fi
-#
-# if ! diff downloaded in_repo; then
-#   echo "Error: vendor files are not the same"
-#   echo "Please run 'vendor.sh' and commit the changes"
-#   exit 1
-# fi
-
-find ./vendor/ -name '.cargo-checksum.json' -exec md5sum {} + | sort -k 2 | cut -d" " -f1 >downloaded
-find ./repo_vendor/vendor/ -name '.cargo-checksum.json' -exec md5sum {} + | sort -k 2 | cut -d" " -f1 >in_repo
+find ./vendor -name 'Cargo.toml' -exec cargo generate-lockfile --manifest-path {} \;
+find ./vendor -name 'Cargo.toml' -exec cargo pkgid --manifest-path {} \; | rev | cut -d'/' -f1 | rev | sort >downloaded
+find ./repo_vendor -name 'Cargo.toml' -exec cargo generate-lockfile --manifest-path {} \;
+find ./repo_vendor -name 'Cargo.toml' -exec cargo pkgid --manifest-path {} \; | rev | cut -d'/' -f1 | rev | sort >in_repo
 
 if [ ! -f downloaded ] || [ ! -f in_repo ]; then
   echo "Error: downloaded or in_repo file not found"
@@ -28,7 +16,7 @@ if [ ! -f downloaded ] || [ ! -f in_repo ]; then
 fi
 
 if ! diff downloaded in_repo; then
-  echo "Error: vendor files are not the same"
+  echo "Error: vendored dependencies are not the same"
   echo "Please run 'vendor.sh' and commit the changes"
   exit 1
 fi
