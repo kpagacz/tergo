80f9e15b075c8bdfb8b4cac54b27184efb914aa4
Konrad Pagacz <konrad.pagacz@gmail.com>
2024-11-24 11:47:45 +0100

    another try

--- antidotum/tergo/src/rust/check_vendor_updates.sh
+++ antidotum/tergo/src/rust/check_vendor_updates.sh
@@ -5,8 +5,22 @@ cargo vendor
 mkdir repo_vendor
 tar -xvf vendor.tar.xz -C repo_vendor
 
-find ./vendor/ -type f -exec md5sum {} + | sort -k 2 | cut -d" " -f1 >downloaded
-find ./repo_vendor/vendor/ -type f -exec md5sum {} + | sort -k 2 | cut -d" " -f1 >in_repo
+# find ./vendor/ -type f -exec md5sum {} + | sort -k 2 | cut -d" " -f1 >downloaded
+# find ./repo_vendor/vendor/ -type f -exec md5sum {} + | sort -k 2 | cut -d" " -f1 >in_repo
+#
+# if [ ! -f downloaded ] || [ ! -f in_repo ]; then
+#   echo "Error: downloaded or in_repo file not found"
+#   exit 1
+# fi
+#
+# if ! diff downloaded in_repo; then
+#   echo "Error: vendor files are not the same"
+#   echo "Please run 'vendor.sh' and commit the changes"
+#   exit 1
+# fi
+
+find ./vendor/ -name '.cargo-checksum.json' -exec md5sum {} + | sort -k 2 | cut -d" " -f1 >downloaded
+find ./repo_vendor/vendor/ -name '.cargo-checksum.json' -exec md5sum {} + | sort -k 2 | cut -d" " -f1 >in_repo
 
 if [ ! -f downloaded ] || [ ! -f in_repo ]; then
   echo "Error: downloaded or in_repo file not found"
